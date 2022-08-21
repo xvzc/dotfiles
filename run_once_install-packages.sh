363aabe72e57c97a976a9c0543c913d1b1106f3a
xvzc <dev.kwanghoo@gmail.com>
2022-08-21 18:06:27 +0900

    update

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 mkdir -p ~/.zsh;
 mkdir -p ~/.config/nvim;
+mkdir -p ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git\ 
-  ~/.zsh/zsh-syntax-highlighting;
-
-
-# install packer.nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\ 
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim;
+	~/.zsh/zsh-syntax-highlighting;
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -18,6 +14,11 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f .tmux/.tmux.conf
 
+# install packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+	~/.local/share/nvim/site/pack/packer/start/packer.nvim;
+
+
 # Linux specific
 {{ if eq .chezmoi.os "linux" -}}
 #!/bin/sh
@@ -26,7 +27,6 @@ sudo yay -Sy ripgrep;
 sudo yay -Sy fd;
 sudo yay -Sy tmux;
 
-# MacOs specific
 {{ else if eq .chezmoi.os "darwin" -}}
 #!/bin/sh
 
