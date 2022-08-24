9ddba727f37131ec004fabddf9320109e2176763
xvzc <dev.kwanghoo@gmail.com>
2022-08-25 01:49:24 +0900

    add

---
+++ dot_config/nvim/lua/settings/inident-blankline.lua
@@ -0,0 +1,12 @@
+require("indent_blankline").setup {
+    char = "",
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
+    space_char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
+    show_trailing_blankline_indent = false,
+}
