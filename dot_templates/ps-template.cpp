b1e2c24d7c782c049653646246f0cec4779ae923
xvzc <dev.kwanghoo@gmail.com>
2022-12-26 16:19:19 +0900

    fix: ps-template

--- dot_templates/ps-template.cpp
+++ dot_templates/ps-template.cpp
@@ -26,8 +26,7 @@ using namespace std;
 /* ----------------------------------------------- */
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(false); cin.tie(0);
 
     if constexpr (local) 
         (void)!freopen("input.txt", "r", stdin);
