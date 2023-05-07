ecf505a1dc33a4866a3a809c09225392635011a8
xvzc <dev.kwanghoo@gmail.com>
2023-05-07 15:01:57 +0900

    chore: Add wakeonlan

--- dot_scripts/wake-on-lan.pl
+++ dot_scripts/wake-on-lan.pl
@@ -283,95 +283,51 @@ exit 0;
 
 
 __END__
-
-
 # Script documentation
 =encoding utf8
-
 =head1 NAME
-
 wakeonlan - Perl script to wake up computers
-
 =head1 SYNOPSIS
-
 wakeonlan [-h|--help] [-v|--version] [-q|--quiet] [-n|--dry-run] [-i|--ip IP_address] [-p|--port port] [-f|--file file_name] [[hardware_address] ...]
-
 =head1 DESCRIPTION
-
 This script sends 'magic packets' to wake-on-lan enabled ethernet adapters and motherboards, in order to switch on the called PC. Be sure to connect the NIC with the motherboard if necessary, and to enable the WOL function in the BIOS.
-
 The 'magic packet' consists of 6 times 0xFF followed by 16 times the hardware address of the NIC. This sequence can be encapsulated in any kind of packet. This script uses UDP packets.
-
 =head1 OPTIONS
-
 =over
-
 =item B<-h, --help>
-
 Displays the help information.
-
 =item B<-v, --version>
-
 Displays the script version.
-
 =item B<-i, --ip=IP_address>
-
 Destination IP address. Unless you have static ARP tables you should
 use some kind of broadcast address (the broadcast address of the network where the computer resides or the limited broadcast address). Default: 255.255.255.255 (the limited broadcast address).
-
 =item B<-p, --port=port>
-
 Destination port. Default: 9 (the discard port).
-
 =item B<-f, --file=file_name>
-
 File with hardware addresses of wakeable computers. For an example check
 the file lab001.wol in the examples subdirectory.
-
 =item B<-q, --quiet>
-
 Quiet mode.
-
 =item B<-n, --dry-run>
-
 Print the commands that would be executed, but do not execute them.
-
 =back
-
 =head1 EXAMPLES
-
 Using the limited broadcast address (255.255.255.255):
-
     $ wakeonlan 01:02:03:04:05:06
     $ wakeonlan 01:02:03:04:05:06 01:02:03:04:05:07
-
 Using a subnet broadcast address:
-
     $ wakeonlan -i 192.168.1.255 01:02:03:04:05:06
-
 Using another destination port:
-
     $ wakeonlan -i 192.168.1.255 -p 1234 01:02:03:04:05:06
-
 Using a file as source of hardware and IP addresses:
-
     $ wakeonlan -f examples/lab001.wol
     $ wakeonlan -f examples/lab001.wol 01:02:03:04:05:06
-
 =head1 AUTHOR
-
 José Pedro Oliveira <jose.p.oliveira.oss@gmail.com> maintaining and expanding original work done by Ico Doornekamp <ico@edd.dhs.org>.
-
 =head1 COPYRIGHT
-
 Copyright (c) 2000-2021 José Pedro Oliveira.
-
 This is free software.  You may modify it and distribute it under Perl's Artistic License.  Modified versions must be clearly indicated.
-
 =head1 SEE ALSO
-
 For more information regarding this script and Wakeonlan technology just check the project page at https://github.com/jpoliv/wakeonlan.
-
 =cut
-
 # vim:set ai ts=4 sw=4 sts=4 syntax=perl:
