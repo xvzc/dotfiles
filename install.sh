549d9f07a8918b7bb1eb1ed9a00fa30018d67370
xvzc <45588457+xvzc@users.noreply.github.com>
2024-01-04 23:58:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,20 +10,21 @@ esac
 echo "Detected OS: ${machine}"
 
 if [ "$machine" == "Mac" ]; then
-    source ~/.local/share/chezmoi/setup/macos-setup.sh
-    all()
-    
+
     if type brew > /dev/null 2>&1; then
         echo "Found brew at $(whareis brew). Skipping install."
     else
         echo "homdbrew not found. installing.."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" && \
-	eval "$(/opt/homebrew/bin/brew shellenv)"
+	    eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 
     echo "Installing MacOS packages.."
-    brew bundle --file=~/yadmfile/Brewfile
-    
+    brew bundle --file=~/yadmfile/Brewfile && \
+        echo "Successfully installed packaged" || \
+	(echo "Failed to install packages" && exit 1)
+
+    chezmoi init github.com/xvzc/dotfiles && chezmoi apply -R
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Installing Linux packages.."
     yay -S --needed --noconfirm - < ~/.local/share/chezmoi/aur_packages.txt
@@ -42,4 +43,4 @@ else
    exit 1;
 fi
 
-chezmoi apply -R
+
