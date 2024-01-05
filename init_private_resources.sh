f2145c8abb7e7f59f743b4bb88c298e78e36604d
xvzc <dev.kwnaghoo@gmail.com>
2024-01-06 02:59:47 +0900

    update

--- init_private_resources.sh
+++ init_private_resources.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-op read "op://Personal/SSH Github xvzc/public key" > ~/.ssh/xvzc.pub && \
-  chmod 600 ~/.ssh/xvzc.pub
+CUR_OS=$(uname)
 
-op read "op://Personal/SSH Arch Linux/public key" > ~/.ssh/arch.pub && \
-  chmod 600 ~/.ssh/arch.pub
-
-NVIM_DIR="$HOME/.config/nvim"
-if [ -d "$NVIM_DIR/.git" ]; then
-  echo "'$NVIM_DIR/.git' already exists. setting remote url"
-  git --git-dir "$NVIM_DIR/.git" remote set-url origin git@xvzc.github.com:xvzc/nvim.git
-else
-  rm -rf "$NVIM_DIR"
-  git clone git@xvzc.github.com:xvzc/nvim.git $NVIM_DIR
+if ! brew bundle --no-lock --no-upgrade --file=~/.local/share/chezmoi/setup/Brewfile.personal; then
+	echo "Filed to install packages"
+	exit 1
 fi
+
+op read "op://Personal/SSH Github xvzc/public key" >~/.ssh/xvzc.pub &&
+	chmod 600 ~/.ssh/xvzc.pub
+
+op read "op://Personal/SSH Arch Linux/public key" >~/.ssh/arch.pub &&
+	chmod 600 ~/.ssh/arch.pub
 
 ASSETS_DIR="$HOME/.config/assets"
 if [ -d "$ASSETS_DIR/.git" ]; then
-  echo "'$ASSETS_DIR/.git' already exists. setting remote url"
-  git --git-dir "$ASSETS_DIR/.git" remote set-url origin git@xvzc.github.com:xvzc/assets.git
+	echo "'$ASSETS_DIR/.git' already exists."
+	git --git-dir "$ASSETS_DIR/.git" remote set-url origin git@xvzc.github.com:xvzc/assets.git
 else
-  rm -rf "$ASSETS_DIR"
-  git clone git@xvzc.github.com:xvzc/assets.git $ASSETS_DIR
+	git clone git@xvzc.github.com:xvzc/assets.git $ASSETS_DIR
 fi
 
-_is_darwin && cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Light.ttf ~/Library/Fonts/
-_is_darwin && cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Regular.ttf ~/Library/Fonts/
+# Install fonts
+if [ "$CUR_OS" = "Darwin" ]; then
+	cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Light.ttf ~/Library/Fonts/
+	cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Regular.ttf ~/Library/Fonts/
+elif [ "$CUR_OS" = "Linux" ]; then
+	cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Light.ttf ~/.local/share/fonts/
+	cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Regular.ttf ~/.local/share/fonts/
+fi
 
-_is_linux && cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Light.ttf ~/.local/share/fonts/
-_is_linux && cp ~/.config/assets/fonts/hesalche/ttf/hesalche-Regular.ttf ~/.local/share/fonts/
+# install cargo
+curl https://sh.rustup.rs -sSf | sh
