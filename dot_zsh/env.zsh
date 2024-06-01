573542644d1181de9a2e7cbfaab7e0254039cc19
xvzc <dev.kwanghoo@gmail.com>
2024-06-01 16:21:53 +0900

    update

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -17,16 +17,3 @@ export EDITOR=nvim
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.scripts/git:$PATH"
 
-# ┌─────────────────┐ 
-# │ CONDITIONAL ENV │ 
-# └─────────────────┘ 
-_is_darwin && \
-  export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-
-_is_darwin && export PATH="/opt/homebrew/bin:$PATH"
-
-_is_linux && export SSH_AUTH_SOCK=~/.1password/agent.sock
-
-command -v go &> /dev/null && export PATH=`go env GOPATH`/bin:$PATH
-command -v bob &> /dev/null && export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
-
