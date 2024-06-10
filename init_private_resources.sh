4b89a90c4216a80e4bfa3654611c569b8691a912
xvzc <dev.kwanghoo@gmail.com>
2024-06-11 00:25:28 +0900

    update

--- init_private_resources.sh
+++ init_private_resources.sh
@@ -14,17 +14,26 @@ check-1password() {
 		open -a 1Password
 		error "Login 1password app"
 	fi
-
-	if ! ssh-add -l &>/dev/null; then
-		open -a 1Password
-		error "Setup 1password ssh-agent"
-	fi
 }
 
 check-1password
 
-op read "op://Personal/SSH Github xvzc/public key" >~/.ssh/xvzc.pub &&
+op read --force \
+  "op://mbqg52gjbireiglzwrrck5nbcm/br6vmskxg3yyesbwjfv2zev46y/public key" \
+  | tr -d "\015" > ~/.ssh/xvzc.pub && \
 	chmod 600 ~/.ssh/xvzc.pub
 
-op read "op://Personal/SSH Arch Linux/public key" >~/.ssh/arch.pub &&
-	chmod 600 ~/.ssh/arch.pub
+op read --force \
+  "op://mbqg52gjbireiglzwrrck5nbcm/br6vmskxg3yyesbwjfv2zev46y/private key?ssh-format=openssh" \
+  | tr -d '\015' > ~/.ssh/xvzc && \
+  chmod 600 ~/.ssh/xvzc
+
+op read --force \
+  "op://mbqg52gjbireiglzwrrck5nbcm/6dnb4xqgeh2voofslw3dr35dw4/public key" \
+  | tr -d '\015' > ~/.ssh/arch.pub && \
+  chmod 600 ~/.ssh/arch.pub
+
+op read --force \
+  "op://mbqg52gjbireiglzwrrck5nbcm/6dnb4xqgeh2voofslw3dr35dw4/private key?ssh-format=openssh" \
+  | tr -d '\015' > ~/.ssh/arch && \
+  chmod 600 ~/.ssh/arch
