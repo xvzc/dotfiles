12bd8f41b8f4eebc3641100b1653f1326425dc2e
xvzc <dev.kwanghoo@gmail.com>
2024-06-24 13:42:49 +0900

    Update .zshrc Change attributes of .zsh/util/func.zsh Change attributes of .zsh/util/fzf.zsh

--- dot_zsh/util/fzf.zsh
+++ dot_zsh/util/fzf.zsh
@@ -12,29 +12,8 @@ export FZF_DEFAULT_OPTS="--layout=reverse \
 
 export FZF_TMUX_OPTS='-p80%,60%'
 
-# export FZF_CUSTOM_OPTS=( \
-#   --layout=reverse \
-#   --inline-info \
-#   --height 40%  \
-#   --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899' \
-# )
-
 # Use ~~ as the trigger sequence instead of the default **
 export FZF_COMPLETION_TRIGGER='**'
-
-# Options to fzf command
-# export ='--border --info=inline'
-# export FZF_COMPLETION_OPTS="--layout=reverse \
-#                             --inline-info \
-#                             --height 40%  \
-#                             --preview 'bat --style=numbers --color=always --line-range :500 {}' \
-#                             --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899' \
-#                            "
-
-# Use fd (https://github.com/sharkdp/fd) instead of the default find
-# command for listing path candidates.
-# - The first argument to the function ($1) is the base path to start traversal
-# - See the source code (completion.{bash,zsh}) for the details.
 
 _fzf_compgen_path() {
   fd --hidden --follow --exclude ".git" . "$1"
@@ -102,16 +81,5 @@ function fif() {
   --bind '?:toggle-preview,alt-w:toggle-preview-wrap'\
   --bind 'alt-v:execute(nvim {} >/dev/tty </dev/tty)'\
   --preview "bat --theme='OneHalfDark' --style=numbers --color=always {} | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
-}
-
-git-commit-show () {
-  git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr"  | \
-   fzf --ansi --no-sort --reverse --tiebreak=index --preview \
-   'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
-   --bind "j:down,k:up,alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up,q:abort,ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {}
-FZF-EOF" --preview-window=right:60%
 }
 
