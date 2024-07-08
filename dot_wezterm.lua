3f6fdc6b2a6a8047b7a628a5eba6e8e3aa480f9b
xvzc <dev.kwanghoo@gmail.com>
2024-07-08 14:54:07 +0900

    chore: add terminfo for wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -3,6 +3,8 @@ local wezterm = require("wezterm")
 
 local	config = wezterm.config_builder()
 
+config.term = 'wezterm'
+
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
