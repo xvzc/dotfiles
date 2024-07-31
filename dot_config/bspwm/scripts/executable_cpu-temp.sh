a24e39d056776db610248783ef3b452584f5de12
xvzc <dev.kwanghoo@gmail.com>
2024-07-31 15:48:03 +0900

    No commit message

--- dot_config/bspwm/scripts/executable_cpu-temp.sh
+++ dot_config/bspwm/scripts/executable_cpu-temp.sh
@@ -3,3 +3,7 @@
 if [[ "$(lscpu | grep 'Vendor ID:' | awk '{print $3}' | grep -q 'AMD')" -eq 0 ]]; then
   sensors | grep "Tctl:" | tr -d '+' | awk '{print $2}'
 fi
+
+if [[ "$(lscpu | grep 'Vendor ID:' | awk '{print $3}' | grep -q 'Intel')" -eq 0 ]]; then
+  sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}'
+fi
