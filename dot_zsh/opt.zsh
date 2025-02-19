44e885554f2e28cc034b47fb377609b7feaecb6f
xvzc <dev.kwanghoo@gmail.com>
2025-02-20 04:47:46 +0900

    update

--- dot_zsh/opt.zsh
+++ dot_zsh/opt.zsh
@@ -10,3 +10,15 @@ setopt pushdminus  # Invert meanings of +N and -N arguments to pushd
 # Enable hex color codes
 zmodload zsh/nearcolor
 
+HISTFILE="$HOME/.zsh_history" # location of the history file
+SAVEHIST=1000
+HISTSIZE=1200 # current session's history limit, also following this https://unix.stackexchange.com/a/595475 $HISTSIZE should be at least 20% bigger than $SAVEHIST 
+
+setopt INC_APPEND_HISTORY # history file is updated immediately after a command is entered
+setopt SHARE_HISTORY # allows multiple Zsh sessions to share the same command history 
+setopt EXTENDED_HISTORY # records the time when each command was executed
+setopt APPENDHISTORY # ensures that each command entered in the current session is appended to the history file immediately after execution
+
+
+
+
